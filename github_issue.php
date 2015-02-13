3ac65a8850ad04bbafe2ecde24dc0b81b022ef2b
Michael <michael@orientationsys.com>
2015-02-13 14:59:07 +0800

    first commit

---
+++ github_issue.php
@@ -0,0 +1,12 @@
+<?php  
+
+/**
+ *
+ * @package  core php  
+ * @author   Michael Cheng <michael@orientationsys.com>
+ * @description 
+ *  Use it as a githhub webhook script to send issue activities notification email
+ *  to specific email addresses
+ */
+ 
+ ?>
