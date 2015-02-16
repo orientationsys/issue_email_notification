f777b47e51a7961288bd6ac19c69850bae89f10a
orientationsys <michael@orientationsys.com>
2015-02-16 10:31:27 +0800

    change to use responsive email template now
    Signed-off-by: orientationsys <michael@orientationsys.com>

--- github_issue.php
+++ github_issue.php
@@ -57,7 +57,7 @@ if(!$mail->send()) {
  
  $emailAddresses = array(
 	'tttccc@gmail.com',
-    'lz@orientationsys.com'
+    //'lz@orientationsys.com'
 );
 
 
@@ -103,10 +103,10 @@ if($objPayload->action === 'created'){
 		foreach($match[0] as $image){
 			$pattern_url='/((?:[a-z][a-z]+))(:)(\/)((?:\/[\w\.\-]+)+)/is';
 			preg_match_all($pattern_url,$image,$image_url);
-			$subjectPara4 = '<strong>"'.str_replace($image, '<br><img src="'.$image_url[0][0].'"/><br>',$objPayload->comment->body).'"</strong>';	
+			$subjectPara4 = '<span style="color:#244F79">"'.str_replace($image, '<br><img src="'.$image_url[0][0].'"/><br>',$objPayload->comment->body).'"</strong>';	
 		}
 	}else{
-		$subjectPara4 = '<strong>"'.$objPayload->comment->body.'"</strong>';		
+		$subjectPara4 = '<span style="color:#244F79">"'.$objPayload->comment->body.'"</strong>';		
 	}
 	
 	$subjectPara5 = $objPayload->comment->updated_at;
@@ -132,16 +132,16 @@ elseif($objPayload->action !='labeled'){
 		foreach($match[0] as $image){
 			$pattern_url='/((?:[a-z][a-z]+))(:)(\/)((?:\/[\w\.\-]+)+)/is';
 			preg_match_all($pattern_url,$image,$image_url);
-			$subjectPara4 = '<strong>"'.str_replace($image, '<br><img src="'.$image_url[0][0].'"/><br>',$objPayload->issue->body).'"</strong>';	
+			$subjectPara4 = '<span style="color:#244F79">"'.str_replace($image, '<br><img src="'.$image_url[0][0].'"/><br>',$objPayload->issue->body).'"</strong>';	
 		}
 	}else{
-		$subjectPara4 = '<strong>"'.$objPayload->issue->body.'"</strong>';		
+		$subjectPara4 = '<span style="color:#244F79">"'.$objPayload->issue->body.'"</strong>';		
 	}
 	$subjectPara5 = $objPayload->issue->updated_at;
 }else{
 	exit(0);
 }
-$message = '<html>'.
+/*$message = '<html>'.
 '<head>'.
 '<meta http-equiv="content-type" content="text/html; charset=utf-8">'.
 '<title>Email notification</title>'.
@@ -164,6 +164,204 @@ $message = '<html>'.
 
 
 '</body></html>';
+*/
+
+$message = <<< EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Simples-Minimalistic Responsive Template</title>
+      
+      <style type="text/css">
+         /* Client-specific Styles */
+         #outlook a {padding:0;} /* Force Outlook to provide a "view in browser" menu link. */
+         body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
+         /* Prevent Webkit and Windows Mobile platforms from changing default font sizes, while not breaking desktop design. */
+         .ExternalClass {width:100%;} /* Force Hotmail to display emails at full width */
+         .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;} /* Force Hotmail to display normal line spacing.*/
+         #backgroundTable {margin:0; padding:0; width:100% !important; line-height: 100% !important;}
+         img {outline:none; text-decoration:none;border:none; -ms-interpolation-mode: bicubic;}
+         a img {border:none;}
+         .image_fix {display:block;}
+         p {margin: 0px 0px !important;}
+         table td {border-collapse: collapse;}
+         table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
+         a {color: #0a8cce;text-decoration: none;text-decoration:none!important;}
+         /*STYLES*/
+         table[class=full] { width: 100%; clear: both; }
+         /*IPAD STYLES*/
+         @media only screen and (max-width: 640px) {
+         a[href^="tel"], a[href^="sms"] {
+         text-decoration: none;
+         color: #0a8cce; /* or whatever your want */
+         pointer-events: none;
+         cursor: default;
+         }
+         .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+         text-decoration: default;
+         color: #0a8cce !important;
+         pointer-events: auto;
+         cursor: default;
+         }
+         table[class=devicewidth] {width: 440px!important;text-align:center!important;}
+         table[class=devicewidthinner] {width: 420px!important;text-align:center!important;}
+         img[class=banner] {width: 440px!important;height:220px!important;}
+         img[class=colimg2] {width: 440px!important;height:220px!important;}
+         
+         
+         }
+         /*IPHONE STYLES*/
+         @media only screen and (max-width: 480px) {
+         a[href^="tel"], a[href^="sms"] {
+         text-decoration: none;
+         color: #0a8cce; /* or whatever your want */
+         pointer-events: none;
+         cursor: default;
+         }
+         .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+         text-decoration: default;
+         color: #0a8cce !important; 
+         pointer-events: auto;
+         cursor: default;
+         }
+         table[class=devicewidth] {width: 280px!important;text-align:center!important;}
+         table[class=devicewidthinner] {width: 260px!important;text-align:center!important;}
+         img[class=banner] {width: 280px!important;height:140px!important;}
+         img[class=colimg2] {width: 280px!important;height:140px!important;}
+         td[class=mobile-hide]{display:none!important;}
+         td[class="padding-bottom25"]{padding-bottom:25px!important;}
+        
+         }
+      </style>
+   </head>
+   <body>
+
+<!-- Start of seperator -->
+<table width="100%" bgcolor="#ffffff" cellpadding="0" cellspacing="0" border="0" id="backgroundTable" st-sortable="seperator">
+   <tbody>
+      <tr>
+         <td>
+            <table width="800" align="center" cellspacing="0" cellpadding="0" border="0" class="devicewidth">
+               <tbody>
+                  <tr>
+                     <td align="center" height="30" style="font-size:1px; line-height:1px;">&nbsp;</td>
+                  </tr>
+                  <tr>
+                     <td width="550" align="center" height="1" bgcolor="#d1d1d1" style="font-size:1px; line-height:1px;">&nbsp;</td>
+                  </tr>
+                  <tr>
+                     <td align="center" height="30" style="font-size:1px; line-height:1px;">&nbsp;</td>
+                  </tr>
+               </tbody>
+            </table>
+         </td>
+      </tr>
+   </tbody>
+</table>
+<!-- End of seperator -->   
+<!-- Start Full Text -->
+<table width="100%" bgcolor="#ffffff" cellpadding="0" cellspacing="0" border="0" id="backgroundTable" st-sortable="full-text">
+   <tbody>
+      <tr>
+         <td>
+            <table width="800" cellpadding="0" cellspacing="0" border="0" align="center" class="devicewidth">
+               <tbody>
+                  <tr>
+                     <td width="100%">
+                        <table width="800" cellpadding="0" cellspacing="0" border="0" align="center" class="devicewidth">
+                           <tbody>
+                              <!-- Spacing -->
+                              <tr>
+                                 <td height="5" style="font-size:1px; line-height:1px; mso-line-height-rule: exactly;">&nbsp;</td>
+                              </tr>
+                              <!-- Spacing -->
+                              <tr>
+                                 <td>
+                                    <table width="800" align="center" cellpadding="0" cellspacing="0" border="0" class="devicewidthinner">
+                                       <tbody>
+                                          <!-- Title -->
+                                          <tr>
+                                             <td style="font-family: Helvetica, arial, sans-serif; font-size: 30px; color: #333333; text-align:center; line-height: 30px;" st-title="fulltext-title">
+											 {$subjectPara1}
+                                             </td>
+                                          </tr>
+                                          <!-- End of Title -->
+                                          <!-- spacing -->
+                                          <tr>
+                                             <td width="100%" height="20" style="font-size:1px; line-height:1px; mso-line-height-rule: exactly;">&nbsp;</td>
+                                          </tr>
+                                          <!-- End of spacing -->
+                                          <!-- content -->
+										  <tr>
+											<td style="font-family: Helvetica, arial, sans-serif; font-size: 16px; color: #666666; text-align:center; line-height: 30px;" st-content="fulltext-content">
+												<p>{$subjectPara2}</p>
+												<p>{$subjectPara3}</p>
+											</td>
+										  </tr>
+                                          <tr>
+                                             <td style="font-family: Helvetica, arial, sans-serif; font-size: 16px; color: #666666; text-align:left; line-height: 30px;" st-content="fulltext-content">
+                                                
+												<p>{$subjectPara4}</p>
+												
+                                             </td>
+                                          </tr>
+										  <tr>
+											<td style="font-family: Helvetica, arial, sans-serif; font-size: 14px; color: #666666; text-align:right; line-height: 30px;" st-content="fulltext-content">
+												<p>{$subjectPara5}</p>
+											</td>
+										  </tr>
+                                          <!-- End of content -->
+                                       </tbody>
+                                    </table>
+                                 </td>
+                              </tr>
+                              <!-- Spacing -->
+                              <tr>
+                                 <td height="20" style="font-size:1px; line-height:1px; mso-line-height-rule: exactly;">&nbsp;</td>
+                              </tr>
+                              <!-- Spacing -->
+                           </tbody>
+                        </table>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </td>
+      </tr>
+   </tbody>
+</table>
+<!-- end of full text -->
+<!-- Start of seperator -->
+<table width="100%" bgcolor="#ffffff" cellpadding="0" cellspacing="0" border="0" id="backgroundTable" st-sortable="seperator">
+   <tbody>
+      <tr>
+         <td>
+            <table width="800" align="center" cellspacing="0" cellpadding="0" border="0" class="devicewidth">
+               <tbody>
+                  <tr>
+                     <td align="center" height="30" style="font-size:1px; line-height:1px;">&nbsp;</td>
+                  </tr>
+                  <tr>
+                     <td width="800" align="center" height="1" bgcolor="#d1d1d1" style="font-size:1px; line-height:1px;">&nbsp;</td>
+                  </tr>
+                  <tr>
+                     <td align="center" height="30" style="font-size:1px; line-height:1px;">&nbsp;</td>
+                  </tr>
+               </tbody>
+            </table>
+         </td>
+      </tr>
+   </tbody>
+</table>
+<!-- End of seperator -->  
+
+   
+   </body>
+   </html>
+EOF;
+
 
 /*EMAIL TEMPLATE ENDS*/
 
